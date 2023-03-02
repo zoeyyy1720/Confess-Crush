d53adc3bdd50fe5a33bd4b4141364c7c12dc48d6
zoeyyy1720 <126740671+zoeyyy1720@users.noreply.github.com>
2023-03-01 22:22:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 const CONFIG = {
     titleWeb: "EMERGENCY",
     introTitle: 'Hi baby ><',
-    introDesc: ` Im sorry for making you mad :<
-    Im sorry for yelling at you 
-    `,
-    btnIntro: 'CLICK HERE,'
+    introDesc: `Im sorry for yelling at you `,
+    btnIntro: 'CLICK HERE',
     title: 'I LOVE U SO MUCH🥰',
     desc: 'FORGIVE ME> :< 🥺️ 💓 ',
     btnYes: 'YAY 🥳️',
