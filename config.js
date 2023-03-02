f86895e2586a44a14730e93c51d57abb8565211c
zoeyyy1720 <126740671+zoeyyy1720@users.noreply.github.com>
2023-03-01 22:00:29 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Hi baby >,<',
+    introDesc: `Im sorry for making you mad :<
+    Im sorry for yelling at you 
+    `,
+    btnIntro: 'CLICK HERE,
+    title: 'I LOVE U SO MUCH🥰',
+    desc: 'FORGIVE ME> :< 🥺️ 💓 ',
+    btnYes: 'YAY 🥳️',
+    btnNo: 'NAY 😭',
+    question: 'ARE YOU STILL MAD',
+    btnReply: 'SUBMIT',
+    reply: 'NO',
+    mess: 'YAYYYY🥰.',
+    messDesc: 'LOVE YOU',
+    btnAccept: 'MWAH',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
